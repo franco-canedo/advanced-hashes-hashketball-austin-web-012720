42b38ce1b29b90467d8d71e79f29e7e2cfd2402e
Franco Canedo <francanedo1@hotmail.com>
2020-01-16 22:48:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -18,9 +18,19 @@ def game_hash
         ]
     }
     :away => {
-      :team_name => 
-      :colors => 
-      :players => 
+      :team_name => "",
+      :colors => [],
+      :players => [
+        {:player_name => }, 
+        {:number => },
+        {:shoe => },
+        {:points => },
+        {:rebounds => },
+        {:assists => },
+        {:steals => },
+        {:blocks => },
+        {:slam_dunks}
+        ]
     }
   }
 end
