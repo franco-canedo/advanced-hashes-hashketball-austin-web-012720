be1dd4f1c094090780f15969799a8042ba21033d
Franco Canedo <francanedo1@hotmail.com>
2020-01-16 22:49:12 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,8 +3,8 @@
 def game_hash 
   hash = {
     :home => {
-      :team_name => "",
-      :colors => [],
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {:player_name => }, 
         {:number => },
