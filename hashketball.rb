ebb20ac60ac885496dfe9355abde0fea644a7dea
Franco Canedo <francanedo1@hotmail.com>
2020-01-16 23:14:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,7 +120,7 @@ end
 def num_scored_points(player)
   hash = game_hash
   hash.each do |team|
-    points = team[:players].each do |player_hash|
+    team[:players].each do |player_hash|
       if player_hash[:player_name] == player
         
       end
